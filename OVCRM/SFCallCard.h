673ebc248e1cc636a0083d22a67f3a567b11298c
bonshington <bonshington@gmail.com>
2012-07-11 22:57:55 +0700

    refractor

---
+++ OVCRM/SFCallCard.h
@@ -0,0 +1,13 @@
+//
+//  SFCallCard.h
+//  OVCRM
+//
+//  Created by Sopon Srisummasheep on 7/11/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import "sObject.h"
+
+@interface SFCallCard : sObject
+
+@end
---
+++ OVCRM/SFCallCard.m
@@ -0,0 +1,13 @@
+//
+//  SFCallCard.m
+//  OVCRM
+//
+//  Created by Sopon Srisummasheep on 7/11/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import "SFCallCard.h"
+
+@implementation SFCallCard
+
+@end
